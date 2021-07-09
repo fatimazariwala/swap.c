f0f9be1f885a9488131f084a1df4a45ac74100ec
fatimazariwala <82675386+fatimazariwala@users.noreply.github.com>
2021-07-09 08:43:07 +0530

    swapping numbers challenge
    ....

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+   int main(){
+       int a=12;
+       int b=13;
+       int temp;
+
+       printf("\nValue of a=%d , Value of b=%d .... Before swap",a,b);
+    temp=a;
+    a=b;
+    b=temp;
+
+
+       printf("\nValue of a=%d , Value of b=%d ..... After swap",a,b);
+
+
+
+    return 0;}
+
